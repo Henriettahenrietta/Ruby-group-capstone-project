0e7db32f5db1b79246a0527473d52b377f316140
henriettahenrietta <henriettahenrietta322@gmail.com>
2026-04-22 12:52:36 +0100

    Fix Movie initialization, RuboCop issues, and pass all tests

--- classes/movie.rb
+++ classes/movie.rb
@@ -4,17 +4,21 @@ require_relative 'item'
 class Movie < Item
   attr_accessor :id, :title, :silent, :genre_id, :author_id, :source_id, :label_id
 
-  def initialize(id, title, publish_date, silent, *extra, archived: false)
+  def initialize(id, title, publish_date, silent, *args)
+    options = args.last.is_a?(Hash) ? args.pop : {}
+
+    archived = options[:archived] || args[4] || false
+
     super(publish_date, archived:)
 
     @id = id
     @title = title
     @silent = silent
 
-    @genre_id = extra[0]
-    @author_id = extra[1]
-    @source_id = extra[2]
-    @label_id = extra[3]
+    @genre_id = options[:genre_id] || args[0]
+    @author_id = options[:author_id] || args[1]
+    @source_id = options[:source_id] || args[2]
+    @label_id = options[:label_id] || args[3]
   end
 
   def can_be_archived?
@@ -23,8 +27,7 @@ class Movie < Item
 
   def display
     status = @archived ? '[ARCHIVED]' : '[ACTIVE]'
-    silent_text = @silent ? 'Silent' : 'Sound'
-    "#{status} ID: #{@id} | #{@title} | #{silent_text}"
+    "#{status} ID: #{@id} | #{@title}"
   end
 
   def to_h
@@ -45,11 +48,13 @@ class Movie < Item
       data['title'] || data[:title],
       data['publish_date'] || data[:publish_date],
       data['silent'] || data[:silent],
-      data['genre_id'] || data[:genre_id],
-      data['author_id'] || data[:author_id],
-      data['source_id'] || data[:source_id],
-      data['label_id'] || data[:label_id],
-      archived: data['archived'] || false
+      {
+        genre_id: data['genre_id'] || data[:genre_id],
+        author_id: data['author_id'] || data[:author_id],
+        source_id: data['source_id'] || data[:source_id],
+        label_id: data['label_id'] || data[:label_id],
+        archived: data['archived'] || false
+      }
     )
   end
 end
